dc2680f93017c4664cc407071fd4e414cef60859
John Kelly <johnkellyoxford@gmail.com>
2022-01-11 16:30:10 +0000

    Split solver and algorithms

--- solver.py
+++ solver.py
@@ -1,87 +1,39 @@
 from collections import defaultdict
 from time import time
 import sudoku
+from enum import Enum
+
+class SolveStrategy(Enum):
+    BACKTRACK = "solveBackTrack"
+    BACKTRACK_SORTED = "solveBackTrackSorted"
 
 class Solver:
-    def __init__(self, Sudoku):
-        self.Sudoku = Sudoku
+    def __init__(self, sudoku):
+        self.sudoku = sudoku
     
     def crimeDesc(self, x, y, n):
         return list(filter(lambda x: x != None, map(lambda arr: arr[1] if n in arr[0]() else None, [
-            [lambda: self.Sudoku.column(x), "Already in column"],
-            [lambda: self.Sudoku.row(y), "Already in row"],
-            [lambda: self.Sudoku.containing_grid(x, y), "Already in grid"],
+            [lambda: self.sudoku.column(x), "Already in column"],
+            [lambda: self.sudoku.row(y), "Already in row"],
+            [lambda: self.sudoku.containing_grid(x, y), "Already in grid"],
         ])))[0]
 
-    def solveBackTrack(self):
+    def solve(self, solver):
         iterations = 0
         lastIterTime = time()
 
-        def stepBack(x, y):
-            if not self.Sudoku.is_given(x, y):
-                visited[x, y] = set()
-                self.Sudoku[x, y] = None
+        def iterate():
+            if iterations % 1_000 == 0:
+                now = time()
+                elapsed = now - lastIterTime
+                lastIterTime = now
 
-            if x > 0:
-                return (x - 1, y)
-            else:
-                assert(y > 0)
-                return (8, y - 1)
+                print(f"{1_000 / elapsed} iterations/sec")
 
-        def stepForward(x, y, value):
-            if value is not None:
-                visited[x, y].add(value)
+        solver.solve(self.sudoku, iterate)
 
-            if x < 8:
-                return (x + 1, y)
-            else:
-                assert(y < 8)
-                return (0, y + 1)
+        assert(self.sudoku.is_solved())
 
-        x = 0
-        y = 0
-
-        visited = defaultdict(set)
-
-        sudoku = self.Sudoku
-
-        while True:
-            x = 0
-            while True:
-                if x == 8 and y == 8 and sudoku.is_solved():
-                    return
-
-                if sudoku.is_given(x, y):
-                    x, y = stepForward(x, y, None)
-                else:
-                    # get current digit if exists
-                    current = sudoku[x, y] or 0
-
-                    for i in range(current, current + 9):
-                        value = i % 9 + 1
-                        if sudoku.is_legal(x, y, value):
-                            sudoku[x, y] = value
-
-                            x, y = stepForward(x, y, value)
-                            iterations += 1
-
-                            if iterations % 1_000 == 0:
-                                now = time()
-                                elapsed = now - lastIterTime
-                                lastIterTime = now
-
-                                print(f"{1_000 / elapsed} iterations/sec")
-
-                            break
-                        else:
-                            visited[x, y].add(value)
-
-                    else: # no legal moves
-                        sudoku[x, y] = None
-                        
-                        x, y = stepBack(x, y)
-                        while sudoku.is_given(x, y) or len(visited[x, y]) == 9:
-                            x, y = stepBack(x, y)
                             
      
 erect_puzzle = sudoku.Sudoku.from_text("""
@@ -99,5 +51,5 @@ erect_puzzle = sudoku.Sudoku.from_text("""
 erect_puzzle = sudoku.example
 
 print(erect_puzzle.highlighted())
-Solver(erect_puzzle).solveBackTrack()
+Solver(erect_puzzle).solve(SolveStrategy.BACKTRACK)
 print(erect_puzzle.highlighted())
